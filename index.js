e2552224fbdfdfcb5936f24c3b51d172fb440bd1
Andy Mitchell <andy.mitchell@hoveringsolutions.com>
2021-09-24 16:54:24 +0200

    delete word added

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(express.json());
 // app.use(requestLogger);
 
 let words = [
-  { enInfinitive: "to walk", esInfinitive: "caminar" },
-  { enInfinitive: "to fly", esInfinitive: "volar" },
+  { id: 1, enInfinitive: "to walk", esInfinitive: "caminar" },
+  { id: 2, enInfinitive: "to fly", esInfinitive: "volar" },
 ];
 
 app.get("/api/words", (request, response) => {
@@ -17,15 +17,23 @@ app.get("/api/words", (request, response) => {
 });
 
 app.post("/api/words", (request, response) => {
-    const body = request.body
-    const newWord = {
-        enInfinitive: body.enInfinitive,
-        esInfinitive: body.esInfinitive,
-    }
-    words = words.concat(newWord)
-    response.status(200);
-  });
-  
+  const body = request.body;
+  const newWord = {
+      id: Number(words.length + 1),
+    enInfinitive: body.enInfinitive,
+    esInfinitive: body.esInfinitive,
+  };
+  console.log(newWord)
+  words = words.concat(newWord);
+  response.status(200);
+});
+
+app.delete("/api/words/:id", (request, response) => {
+  console.log(request.params.id)
+    words = words.filter(w => w.id !== Number(request.params.id))
+    console.log(words)
+    response.status(200)
+});
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });

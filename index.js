9304b5b1899d90600387b4e86f20f732844cea0a
Andy Mitchell <andy.mitchell@hoveringsolutions.com>
2021-09-24 16:36:41 +0200

    Basic adding of words

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 
-app.use(cors())
+app.use(express.static("build"));
+app.use(cors());
 app.use(express.json());
-app.use(requestLogger);
+// app.use(requestLogger);
 
 let words = [
   { enInfinitive: "to walk", esInfinitive: "caminar" },
@@ -15,12 +16,23 @@ app.get("/api/words", (request, response) => {
   response.json(words);
 });
 
+app.post("/api/words", (request, response) => {
+    const body = request.body
+    const newWord = {
+        enInfinitive: body.enInfinitive,
+        esInfinitive: body.esInfinitive,
+    }
+    words = words.concat(newWord)
+    response.status(200);
+  });
+  
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
